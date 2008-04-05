595fb4681293715796f92c76b8de111e0020ed6f
Kevin O'Connor <kevin@koconnor.net>
2008-04-05 10:47:13 -0400

    Add s64 type to types.h.

--- src/types.h
+++ src/types.h
@@ -12,8 +12,9 @@ typedef unsigned short u16;
 typedef signed short s16;
 typedef unsigned int u32;
 typedef signed int s32;
-typedef u32 size_t;
 typedef unsigned long long u64;
+typedef signed long long s64;
+typedef u32 size_t;
 
 #define __VISIBLE __attribute__((externally_visible))
 #ifdef MODE16
