dac46b1400787cdf04d2f3a3671bc78b3584fb71
Kevin O'Connor <kevin@koconnor.net>
2010-02-17 22:49:01 -0500

    Fix off by one error in strtcpy.
    The strtcpy function could overrun its output buffer.

--- src/util.c
+++ src/util.c
@@ -262,7 +262,7 @@ char *
 strtcpy(char *dest, const char *src, size_t len)
 {
     char *d = dest;
-    while (len-- && *src != '\0')
+    while (--len && *src != '\0')
         *d++ = *src++;
     *d = '\0';
     return dest;
