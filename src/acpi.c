20fcf9b099ccc61fd43a166944732329a5fb41b8
Gleb Natapov <gleb@redhat.com>
2011-01-30 14:17:11 +0200

    lets pretend that RTC can be used to wakeup from S4
    WHQL complains otherwise.
    
    Signed-off-by: Gleb Natapov <gleb@redhat.com>

--- src/acpi.c
+++ src/acpi.c
@@ -259,8 +259,8 @@ build_fadt(int bdf)
     fadt->plvl2_lat = cpu_to_le16(0xfff); // C2 state not supported
     fadt->plvl3_lat = cpu_to_le16(0xfff); // C3 state not supported
     pci_init_device(fadt_init_tbl, bdf, fadt);
-    /* WBINVD + PROC_C1 + SLP_BUTTON + FIX_RTC */
-    fadt->flags = cpu_to_le32((1 << 0) | (1 << 2) | (1 << 5) | (1 << 6));
+    /* WBINVD + PROC_C1 + SLP_BUTTON + FIX_RTC + RTC_S4 */
+    fadt->flags = cpu_to_le32((1 << 0) | (1 << 2) | (1 << 5) | (1 << 6) | (1 << 7));
 
     build_header((void*)fadt, FACP_SIGNATURE, sizeof(*fadt), 1);
 
