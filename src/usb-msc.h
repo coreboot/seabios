5208d93bc76a495ff0662d157ffe1699a976e008
Gerd Hoffmann <kraxel@redhat.com>
2012-07-20 10:59:23 +0200

    move usb mass storage defines to usb.h
    With the arrival of usb attached scsi support they
    will be needed outside usb-msd.c too.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- src/usb-msc.h
+++ src/usb-msc.h
@@ -7,15 +7,4 @@ int usb_cmd_data(struct disk_op_s *op, void *cdbcmd, u16 blocksize);
 struct usbdevice_s;
 int usb_msc_init(struct usbdevice_s *usbdev);
 
-
-/****************************************************************
- * MSC flags
- ****************************************************************/
-
-#define US_SC_ATAPI_8020   0x02
-#define US_SC_ATAPI_8070   0x05
-#define US_SC_SCSI         0x06
-
-#define US_PR_BULK         0x50
-
 #endif // ush-msc.h
