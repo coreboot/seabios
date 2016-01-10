9720484aec619d6414bbeed90e203f752742fa5c
Kevin O'Connor <kevin@koconnor.net>
2016-01-10 13:26:26 -0500

    kbd: Refactor capslock and numlock handling
    Simplify the scan_to_keycode[] table by implementing numlock and
    capslock checking in the code.
    
    Signed-off-by: Kevin O'Connor <kevin@koconnor.net>

--- src/kbd.c
+++ src/kbd.c
@@ -278,105 +278,102 @@ handle_16(struct bregs *regs)
 }
 
 #define none 0
-#define MNUM KF0_NUMACTIVE
-#define MCAP KF0_CAPSACTIVE
 
 static struct scaninfo {
     u16 normal;
     u16 shift;
     u16 control;
     u16 alt;
-    u8 lock_flags;
 } scan_to_keycode[] VAR16 = {
-    {   none,   none,   none,   none, none },
-    { 0x011b, 0x011b, 0x011b, 0x0100, none }, /* escape */
-    { 0x0231, 0x0221,   none, 0x7800, none }, /* 1! */
-    { 0x0332, 0x0340, 0x0300, 0x7900, none }, /* 2@ */
-    { 0x0433, 0x0423,   none, 0x7a00, none }, /* 3# */
-    { 0x0534, 0x0524,   none, 0x7b00, none }, /* 4$ */
-    { 0x0635, 0x0625,   none, 0x7c00, none }, /* 5% */
-    { 0x0736, 0x075e, 0x071e, 0x7d00, none }, /* 6^ */
-    { 0x0837, 0x0826,   none, 0x7e00, none }, /* 7& */
-    { 0x0938, 0x092a,   none, 0x7f00, none }, /* 8* */
-    { 0x0a39, 0x0a28,   none, 0x8000, none }, /* 9( */
-    { 0x0b30, 0x0b29,   none, 0x8100, none }, /* 0) */
-    { 0x0c2d, 0x0c5f, 0x0c1f, 0x8200, none }, /* -_ */
-    { 0x0d3d, 0x0d2b,   none, 0x8300, none }, /* =+ */
-    { 0x0e08, 0x0e08, 0x0e7f,   none, none }, /* backspace */
-    { 0x0f09, 0x0f00,   none,   none, none }, /* tab */
-    { 0x1071, 0x1051, 0x1011, 0x1000, MCAP }, /* Q */
-    { 0x1177, 0x1157, 0x1117, 0x1100, MCAP }, /* W */
-    { 0x1265, 0x1245, 0x1205, 0x1200, MCAP }, /* E */
-    { 0x1372, 0x1352, 0x1312, 0x1300, MCAP }, /* R */
-    { 0x1474, 0x1454, 0x1414, 0x1400, MCAP }, /* T */
-    { 0x1579, 0x1559, 0x1519, 0x1500, MCAP }, /* Y */
-    { 0x1675, 0x1655, 0x1615, 0x1600, MCAP }, /* U */
-    { 0x1769, 0x1749, 0x1709, 0x1700, MCAP }, /* I */
-    { 0x186f, 0x184f, 0x180f, 0x1800, MCAP }, /* O */
-    { 0x1970, 0x1950, 0x1910, 0x1900, MCAP }, /* P */
-    { 0x1a5b, 0x1a7b, 0x1a1b,   none, none }, /* [{ */
-    { 0x1b5d, 0x1b7d, 0x1b1d,   none, none }, /* ]} */
-    { 0x1c0d, 0x1c0d, 0x1c0a,   none, none }, /* Enter */
-    {   none,   none,   none,   none, none }, /* L Ctrl */
-    { 0x1e61, 0x1e41, 0x1e01, 0x1e00, MCAP }, /* A */
-    { 0x1f73, 0x1f53, 0x1f13, 0x1f00, MCAP }, /* S */
-    { 0x2064, 0x2044, 0x2004, 0x2000, MCAP }, /* D */
-    { 0x2166, 0x2146, 0x2106, 0x2100, MCAP }, /* F */
-    { 0x2267, 0x2247, 0x2207, 0x2200, MCAP }, /* G */
-    { 0x2368, 0x2348, 0x2308, 0x2300, MCAP }, /* H */
-    { 0x246a, 0x244a, 0x240a, 0x2400, MCAP }, /* J */
-    { 0x256b, 0x254b, 0x250b, 0x2500, MCAP }, /* K */
-    { 0x266c, 0x264c, 0x260c, 0x2600, MCAP }, /* L */
-    { 0x273b, 0x273a,   none,   none, none }, /* ;: */
-    { 0x2827, 0x2822,   none,   none, none }, /* '" */
-    { 0x2960, 0x297e,   none,   none, none }, /* `~ */
-    {   none,   none,   none,   none, none }, /* L shift */
-    { 0x2b5c, 0x2b7c, 0x2b1c,   none, none }, /* |\ */
-    { 0x2c7a, 0x2c5a, 0x2c1a, 0x2c00, MCAP }, /* Z */
-    { 0x2d78, 0x2d58, 0x2d18, 0x2d00, MCAP }, /* X */
-    { 0x2e63, 0x2e43, 0x2e03, 0x2e00, MCAP }, /* C */
-    { 0x2f76, 0x2f56, 0x2f16, 0x2f00, MCAP }, /* V */
-    { 0x3062, 0x3042, 0x3002, 0x3000, MCAP }, /* B */
-    { 0x316e, 0x314e, 0x310e, 0x3100, MCAP }, /* N */
-    { 0x326d, 0x324d, 0x320d, 0x3200, MCAP }, /* M */
-    { 0x332c, 0x333c,   none,   none, none }, /* ,< */
-    { 0x342e, 0x343e,   none,   none, none }, /* .> */
-    { 0x352f, 0x353f,   none,   none, none }, /* /? */
-    {   none,   none,   none,   none, none }, /* R Shift */
-    { 0x372a, 0x372a,   none,   none, none }, /* * */
-    {   none,   none,   none,   none, none }, /* L Alt */
-    { 0x3920, 0x3920, 0x3920, 0x3920, none }, /* space */
-    {   none,   none,   none,   none, none }, /* caps lock */
-    { 0x3b00, 0x5400, 0x5e00, 0x6800, none }, /* F1 */
-    { 0x3c00, 0x5500, 0x5f00, 0x6900, none }, /* F2 */
-    { 0x3d00, 0x5600, 0x6000, 0x6a00, none }, /* F3 */
-    { 0x3e00, 0x5700, 0x6100, 0x6b00, none }, /* F4 */
-    { 0x3f00, 0x5800, 0x6200, 0x6c00, none }, /* F5 */
-    { 0x4000, 0x5900, 0x6300, 0x6d00, none }, /* F6 */
-    { 0x4100, 0x5a00, 0x6400, 0x6e00, none }, /* F7 */
-    { 0x4200, 0x5b00, 0x6500, 0x6f00, none }, /* F8 */
-    { 0x4300, 0x5c00, 0x6600, 0x7000, none }, /* F9 */
-    { 0x4400, 0x5d00, 0x6700, 0x7100, none }, /* F10 */
-    {   none,   none,   none,   none, none }, /* Num Lock */
-    {   none,   none,   none,   none, none }, /* Scroll Lock */
-    { 0x4700, 0x4737, 0x7700,   none, MNUM }, /* 7 Home */
-    { 0x4800, 0x4838,   none,   none, MNUM }, /* 8 UP */
-    { 0x4900, 0x4939, 0x8400,   none, MNUM }, /* 9 PgUp */
-    { 0x4a2d, 0x4a2d,   none,   none, none }, /* - */
-    { 0x4b00, 0x4b34, 0x7300,   none, MNUM }, /* 4 Left */
-    { 0x4c00, 0x4c35,   none,   none, MNUM }, /* 5 */
-    { 0x4d00, 0x4d36, 0x7400,   none, MNUM }, /* 6 Right */
-    { 0x4e2b, 0x4e2b,   none,   none, none }, /* + */
-    { 0x4f00, 0x4f31, 0x7500,   none, MNUM }, /* 1 End */
-    { 0x5000, 0x5032,   none,   none, MNUM }, /* 2 Down */
-    { 0x5100, 0x5133, 0x7600,   none, MNUM }, /* 3 PgDn */
-    { 0x5200, 0x5230,   none,   none, MNUM }, /* 0 Ins */
-    { 0x5300, 0x532e,   none,   none, MNUM }, /* Del */
-    {   none,   none,   none,   none, none },
-    {   none,   none,   none,   none, none },
-    { 0x565c, 0x567c,   none,   none, none }, /* \| */
-    { 0x8500, 0x8700, 0x8900, 0x8b00, none }, /* F11 */
-    { 0x8600, 0x8800, 0x8a00, 0x8c00, none }, /* F12 */
+    {   none,   none,   none,   none },
+    { 0x011b, 0x011b, 0x011b, 0x0100 }, /* escape */
+    { 0x0231, 0x0221,   none, 0x7800 }, /* 1! */
+    { 0x0332, 0x0340, 0x0300, 0x7900 }, /* 2@ */
+    { 0x0433, 0x0423,   none, 0x7a00 }, /* 3# */
+    { 0x0534, 0x0524,   none, 0x7b00 }, /* 4$ */
+    { 0x0635, 0x0625,   none, 0x7c00 }, /* 5% */
+    { 0x0736, 0x075e, 0x071e, 0x7d00 }, /* 6^ */
+    { 0x0837, 0x0826,   none, 0x7e00 }, /* 7& */
+    { 0x0938, 0x092a,   none, 0x7f00 }, /* 8* */
+    { 0x0a39, 0x0a28,   none, 0x8000 }, /* 9( */
+    { 0x0b30, 0x0b29,   none, 0x8100 }, /* 0) */
+    { 0x0c2d, 0x0c5f, 0x0c1f, 0x8200 }, /* -_ */
+    { 0x0d3d, 0x0d2b,   none, 0x8300 }, /* =+ */
+    { 0x0e08, 0x0e08, 0x0e7f,   none }, /* backspace */
+    { 0x0f09, 0x0f00,   none,   none }, /* tab */
+    { 0x1071, 0x1051, 0x1011, 0x1000 }, /* Q */
+    { 0x1177, 0x1157, 0x1117, 0x1100 }, /* W */
+    { 0x1265, 0x1245, 0x1205, 0x1200 }, /* E */
+    { 0x1372, 0x1352, 0x1312, 0x1300 }, /* R */
+    { 0x1474, 0x1454, 0x1414, 0x1400 }, /* T */
+    { 0x1579, 0x1559, 0x1519, 0x1500 }, /* Y */
+    { 0x1675, 0x1655, 0x1615, 0x1600 }, /* U */
+    { 0x1769, 0x1749, 0x1709, 0x1700 }, /* I */
+    { 0x186f, 0x184f, 0x180f, 0x1800 }, /* O */
+    { 0x1970, 0x1950, 0x1910, 0x1900 }, /* P */
+    { 0x1a5b, 0x1a7b, 0x1a1b,   none }, /* [{ */
+    { 0x1b5d, 0x1b7d, 0x1b1d,   none }, /* ]} */
+    { 0x1c0d, 0x1c0d, 0x1c0a,   none }, /* Enter */
+    {   none,   none,   none,   none }, /* L Ctrl */
+    { 0x1e61, 0x1e41, 0x1e01, 0x1e00 }, /* A */
+    { 0x1f73, 0x1f53, 0x1f13, 0x1f00 }, /* S */
+    { 0x2064, 0x2044, 0x2004, 0x2000 }, /* D */
+    { 0x2166, 0x2146, 0x2106, 0x2100 }, /* F */
+    { 0x2267, 0x2247, 0x2207, 0x2200 }, /* G */
+    { 0x2368, 0x2348, 0x2308, 0x2300 }, /* H */
+    { 0x246a, 0x244a, 0x240a, 0x2400 }, /* J */
+    { 0x256b, 0x254b, 0x250b, 0x2500 }, /* K */
+    { 0x266c, 0x264c, 0x260c, 0x2600 }, /* L */
+    { 0x273b, 0x273a,   none,   none }, /* ;: */
+    { 0x2827, 0x2822,   none,   none }, /* '" */
+    { 0x2960, 0x297e,   none,   none }, /* `~ */
+    {   none,   none,   none,   none }, /* L shift */
+    { 0x2b5c, 0x2b7c, 0x2b1c,   none }, /* |\ */
+    { 0x2c7a, 0x2c5a, 0x2c1a, 0x2c00 }, /* Z */
+    { 0x2d78, 0x2d58, 0x2d18, 0x2d00 }, /* X */
+    { 0x2e63, 0x2e43, 0x2e03, 0x2e00 }, /* C */
+    { 0x2f76, 0x2f56, 0x2f16, 0x2f00 }, /* V */
+    { 0x3062, 0x3042, 0x3002, 0x3000 }, /* B */
+    { 0x316e, 0x314e, 0x310e, 0x3100 }, /* N */
+    { 0x326d, 0x324d, 0x320d, 0x3200 }, /* M */
+    { 0x332c, 0x333c,   none,   none }, /* ,< */
+    { 0x342e, 0x343e,   none,   none }, /* .> */
+    { 0x352f, 0x353f,   none,   none }, /* /? */
+    {   none,   none,   none,   none }, /* R Shift */
+    { 0x372a, 0x372a,   none,   none }, /* * */
+    {   none,   none,   none,   none }, /* L Alt */
+    { 0x3920, 0x3920, 0x3920, 0x3920 }, /* space */
+    {   none,   none,   none,   none }, /* caps lock */
+    { 0x3b00, 0x5400, 0x5e00, 0x6800 }, /* F1 */
+    { 0x3c00, 0x5500, 0x5f00, 0x6900 }, /* F2 */
+    { 0x3d00, 0x5600, 0x6000, 0x6a00 }, /* F3 */
+    { 0x3e00, 0x5700, 0x6100, 0x6b00 }, /* F4 */
+    { 0x3f00, 0x5800, 0x6200, 0x6c00 }, /* F5 */
+    { 0x4000, 0x5900, 0x6300, 0x6d00 }, /* F6 */
+    { 0x4100, 0x5a00, 0x6400, 0x6e00 }, /* F7 */
+    { 0x4200, 0x5b00, 0x6500, 0x6f00 }, /* F8 */
+    { 0x4300, 0x5c00, 0x6600, 0x7000 }, /* F9 */
+    { 0x4400, 0x5d00, 0x6700, 0x7100 }, /* F10 */
+    {   none,   none,   none,   none }, /* Num Lock */
+    {   none,   none,   none,   none }, /* Scroll Lock */
+    { 0x4700, 0x4737, 0x7700,   none }, /* 7 Home */
+    { 0x4800, 0x4838,   none,   none }, /* 8 UP */
+    { 0x4900, 0x4939, 0x8400,   none }, /* 9 PgUp */
+    { 0x4a2d, 0x4a2d,   none,   none }, /* - */
+    { 0x4b00, 0x4b34, 0x7300,   none }, /* 4 Left */
+    { 0x4c00, 0x4c35,   none,   none }, /* 5 */
+    { 0x4d00, 0x4d36, 0x7400,   none }, /* 6 Right */
+    { 0x4e2b, 0x4e2b,   none,   none }, /* + */
+    { 0x4f00, 0x4f31, 0x7500,   none }, /* 1 End */
+    { 0x5000, 0x5032,   none,   none }, /* 2 Down */
+    { 0x5100, 0x5133, 0x7600,   none }, /* 3 PgDn */
+    { 0x5200, 0x5230,   none,   none }, /* 0 Ins */
+    { 0x5300, 0x532e,   none,   none }, /* Del */
+    {   none,   none,   none,   none },
+    {   none,   none,   none,   none },
+    { 0x565c, 0x567c,   none,   none }, /* \| */
+    { 0x8500, 0x8700, 0x8900, 0x8b00 }, /* F11 */
+    { 0x8600, 0x8800, 0x8a00, 0x8c00 }, /* F12 */
 };
 
 // Handle a ps2 style scancode read from the keyboard.
@@ -517,16 +514,14 @@ __process_key(u8 scancode)
                    && scancode >= 0x47 && scancode <= 0x53) {
             /* extended keys handling */
             keycode = (scancode << 8) | 0xe0;
-        } else if (flags0 & (KF0_RSHIFT|KF0_LSHIFT)) {
-            /* check if lock state should be ignored because a SHIFT
-             * key is pressed */
-            if (flags0 & GET_GLOBAL(info->lock_flags))
-                keycode = GET_GLOBAL(info->normal);
-            else
-                keycode = GET_GLOBAL(info->shift);
         } else {
-            /* check if lock is on */
-            if (flags0 & GET_GLOBAL(info->lock_flags))
+            u8 useshift = flags0 & (KF0_RSHIFT|KF0_LSHIFT) ? 1 : 0;
+            u8 ascii = GET_GLOBAL(info->normal) & 0xff;
+            if ((flags0 & KF0_NUMACTIVE && scancode >= 0x47 && scancode <= 0x53)
+                || (flags0 & KF0_CAPSACTIVE && ascii >= 'a' && ascii <= 'z'))
+                // Numlock/capslock toggles shift on certain keys
+                useshift ^= 1;
+            if (useshift)
                 keycode = GET_GLOBAL(info->shift);
             else
                 keycode = GET_GLOBAL(info->normal);
