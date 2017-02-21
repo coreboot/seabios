e5bebc77d2a9dfa67cda100b67d787c3ebdb5397
Ben Warren <ben@skyportsystems.com>
2017-02-20 19:56:16 -0800

    romfile-loader: Switch to using named structs
    This is a little cleaner and matches how the command structures are
    defined in QEMU.
    
    Signed-off-by: Ben Warren <ben@skyportsystems.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>

--- src/fw/romfile_loader.h
+++ src/fw/romfile_loader.h
@@ -11,45 +11,45 @@ struct romfile_loader_entry_s {
     u32 command;
     union {
         /*
-         * COMMAND_ALLOCATE - allocate a table from @alloc_file
-         * subject to @alloc_align alignment (must be power of 2)
-         * and @alloc_zone (can be HIGH or FSEG) requirements.
+         * COMMAND_ALLOCATE - allocate a table from @alloc.file
+         * subject to @alloc.align alignment (must be power of 2)
+         * and @alloc.zone (can be HIGH or FSEG) requirements.
          *
          * Must appear exactly once for each file, and before
          * this file is referenced by any other command.
          */
         struct {
-            char alloc_file[ROMFILE_LOADER_FILESZ];
-            u32 alloc_align;
-            u8 alloc_zone;
-        };
+            char file[ROMFILE_LOADER_FILESZ];
+            u32 align;
+            u8 zone;
+        } alloc;
 
         /*
          * COMMAND_ADD_POINTER - patch the table (originating from
-         * @dest_file) at @pointer_offset, by adding a pointer to the table
+         * @dest_file) at @pointer.offset, by adding a pointer to the table
          * originating from @src_file. 1,2,4 or 8 byte unsigned
-         * addition is used depending on @pointer_size.
+         * addition is used depending on @pointer.size.
          */
         struct {
-            char pointer_dest_file[ROMFILE_LOADER_FILESZ];
-            char pointer_src_file[ROMFILE_LOADER_FILESZ];
-            u32 pointer_offset;
-            u8 pointer_size;
-        };
+            char dest_file[ROMFILE_LOADER_FILESZ];
+            char src_file[ROMFILE_LOADER_FILESZ];
+            u32 offset;
+            u8 size;
+        } pointer;
 
         /*
          * COMMAND_ADD_CHECKSUM - calculate checksum of the range specified by
-         * @cksum_start and @cksum_length fields,
+         * @cksum.start and @cksum.length fields,
          * and then add the value at @cksum_offset.
          * Checksum simply sums -X for each byte X in the range
          * using 8-bit math.
          */
         struct {
-            char cksum_file[ROMFILE_LOADER_FILESZ];
-            u32 cksum_offset;
-            u32 cksum_start;
-            u32 cksum_length;
-        };
+            char file[ROMFILE_LOADER_FILESZ];
+            u32 offset;
+            u32 start;
+            u32 length;
+        } cksum;
 
         /* padding */
         char pad[124];
