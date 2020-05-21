coreboot/seabios

commit 29ee1fb85cf07eaa38eba5df49b86419cacc205d

Skip boot menu and timeout with only one boot device
Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
Message-Id: <45aa3ebe-b97c-f1af-2901-ec4e9bcd1084@molgen.mpg.de>
Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

src/boot.c
@@ -670,6 +670,12 @@ interactive_bootmenu(void)
     if (! CONFIG_BOOTMENU || !romfile_loadint("etc/show-boot-menu", 1))
         return;
 
+    // skip menu if only one boot device and no TPM
+    if ((NULL == BootList.first->next) && !tpm_can_show_menu()) {
+       printf("\n");
+       return;
+    }
+
     while (get_keystroke(0) >= 0)
         ;
 

commit c61193d3ec2e29c0764ce93ad17cea259c57faad

boot: Extend `etc/show-boot-menu` to configure skipping boot menu with only one device
Concerns were raised, that skipping the boot menu, if only one device is
present, might make debugging issues more difficult. So, extend the
current runtime configuration option `etc/show-boot-menu` to enable this
feature by setting it to 2.

Fixes: 29ee1fb8 ("Skip boot menu and timeout with only one boot device")
Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>

src/boot.c
@@ -685,12 +685,13 @@ void
 interactive_bootmenu(void)
 {
     // XXX - show available drives?
+    u64 show_boot_menu = romfile_loadint("etc/show-boot-menu", 1);
 
-    if (! CONFIG_BOOTMENU || !romfile_loadint("etc/show-boot-menu", 1))
+    if (! CONFIG_BOOTMENU || show_boot_menu != 0)
         return;
 
     // skip menu if only one boot device and no TPM
-    if ((NULL == BootList.first->next) && !tpm_can_show_menu()) {
+    if ((show_boot_menu == 2) && (NULL == BootList.first->next) && !tpm_can_show_menu()) {
        printf("\n");
        return;
     }